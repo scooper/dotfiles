b19bbb3dc732ea02b0b84357e90464ff70a3896f
Sam Cooper <samuel.j.cooper@outlook.com>
2023-05-29 14:38:38 +0900

    updated script

--- configure.sh
+++ configure.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+overwrite_check () {
+    if [ -d "$1" ] && [ -n "$1" ]; then
+        read -p " - It looks like you already have a configuration in '$1'.
+ - Would you like to overwrite it? (y/n): " yn
+        case ${yn:0:1} in
+            y|Y )
+                return 1 # overwrite
+            ;;
+            * )
+                return 0 # do nothing
+            ;;
+        esac
+    fi
+
+    return 2 # not detected
+}
+
 config_path="/home/$USER/.config"
 nvchad_path="external/NvChad"
 
@@ -8,26 +25,30 @@ if [ ! -f "$nvchad_path/init.lua" ] ; then
     exit
 fi
 
-echo "Setting up NvChad..."
+echo "Setting up nvim..."
 
+overwrite_nvim=$(overwrite_check $config_path/nvim/)
 
-if [ -d "$config_path/nvim" ] ; then
-    echo "It looks like you already have an 'nvim' configuration"
-    overwrite_nvim=true
-    read -p "Would you like to overwrite it? (y/n): " yn
-    case ${yn:0:1} in
-        y|Y )
-            overwrite_nvim=true
-        ;;
-        * )
-            overwrite_nvim=false
-        ;;
-    esac
-
-    if $overwrite_nvim ; then
+if [ $overwrite_nvim=1 ] || [ $overwrite_nvim=2 ] ; then
+    if [ $overwrite_nvim=1 ] ; then
+        echo " - Overwriting..."
         rm -rf "$config_path/nvim"
     fi
+
+    rsync -a --exclude=".*" "$nvchad_path/" "$config_path/nvim/"
+    rsync -a "nvim/" "$config_path/nvim/lua/custom/"
 fi
 
-mkdir "$config_path/nvim"
-cp "$nvchad_path"/* "$config_path/nvim/"
+
+echo "Setting up tmux..."
+
+overwrite_tmux=$(overwrite_check $config_path/tmux/)
+
+if [ $overwrite_tmux=1 ] || [ $overwrite_tmux=2 ]  ; then
+    if [ $overwrite_tmux=1 ] ; then
+        echo " - Overwriting..."
+        rm -rf "$config_path/tmux"
+    fi
+
+    rsync -a "tmux/" "$config_path/tmux"
+fi
